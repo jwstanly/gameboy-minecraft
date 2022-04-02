9730ba4ac0d6208abb402a9eb947e42e0d99d5c5
jwstanly <jwstanly@yahoo.com>
2022-04-02 17:08:42 -0400

    images

---
+++ images/mc-night.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.8
+ * Invocation command was nin10kit mc-night --mode 3 --resize 240x160 mc-night.jpeg 
+ * Time-stamp: Saturday 04/02/2022, 20:41:52
+ * 
+ * Image Information
+ * -----------------
+ * mc-night.jpeg 240@160
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef MCNIGHT_H
+#define MCNIGHT_H
+
+extern const unsigned short mcnight[38400];
+#define MCNIGHT_SIZE 76800
+#define MCNIGHT_LENGTH 38400
+#define MCNIGHT_WIDTH 240
+#define MCNIGHT_HEIGHT 160
+
+#endif
+
---
+++ images/mc-sky.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.8
+ * Invocation command was nin10kit mc-sky --mode 3 --resize 240x160 mc-sky.jpeg 
+ * Time-stamp: Saturday 04/02/2022, 20:40:15
+ * 
+ * Image Information
+ * -----------------
+ * mc-sky.jpeg 240@160
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef MCSKY_H
+#define MCSKY_H
+
+extern const unsigned short mcsky[38400];
+#define MCSKY_SIZE 76800
+#define MCSKY_LENGTH 38400
+#define MCSKY_WIDTH 240
+#define MCSKY_HEIGHT 160
+
+#endif
+
